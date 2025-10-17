527677456dda535ca95be5a8720432e7683aec94
premeika4-design <premeika4@gmail.com>
2025-10-17 18:24:25 +0300

    Create euroleague_fantasy_dashboard.py

---
+++ euroleague_fantasy_dashboard.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="Euroleague Fantasy", page_icon="🏀")
+st.title("Euroleague Fantasy Dashboard")
+st.write("👋 Hello! Your app is running successfully.")
